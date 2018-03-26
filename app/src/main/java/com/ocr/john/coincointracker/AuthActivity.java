c1982a4e56a0df28711a3639431849377c68768c
Boyman10 <contact@yasi.fr>
2018-03-26 22:14:03 +0200

    Working on JUnit and Mockito for bouchonage

---
+++ app/src/main/java/com/ocr/john/coincointracker/AuthActivity.java
@@ -0,0 +1,24 @@
+package com.ocr.john.coincointracker;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+
+
+    public String getUserToken() {
+
+        SharedPreferences prefs = getSharedPreferences("UserToken",MODE_PRIVATE);
+        // lot things to do :::
+
+        return "Waouh";
+
+    }
+}
